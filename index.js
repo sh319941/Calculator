39d73308c01f813384902c889a105492c54bd93c
Xavier <Shine.Xavier@shell.com>
2020-12-17 17:48:44 +0530

    Final File

---
+++ index.js
@@ -0,0 +1,16 @@
+function buttonclick(val)
+{
+ 
+document.getElementById('screen').value +=val;
+
+}
+function clear_data()
+{
+    document.getElementById('screen').value="";
+}
+function equalclick()
+{
+let text=document.getElementById('screen').value;
+let result =eval(text);
+document.getElementById('screen').value=result;
+}
